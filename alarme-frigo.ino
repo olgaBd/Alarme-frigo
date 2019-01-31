8a82d66e1d003baab28479e4e0d77b97df8b5292
Olga Bravard <47146387+olgaBd@users.noreply.github.com>
2019-01-31 11:19:50 +0100

    Add files via upload

---
+++ alarme-frigo.ino
@@ -0,0 +1,22 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin (9600);
+pinMode(7, OUTPUT);
+}
+
+void loop() {
+   int Light = analogRead(0);
+   Serial.println(Light);
+     if(Light>200){
+      delay(10000);
+     if(Light>200){
+   
+   digitalWrite(7,HIGH);
+    }
+    } else{
+   digitalWrite(7,LOW);
+      
+    }
+  
+
+}
